5a052d80b2d11c28fbf4ebcf0fc897a09f6fd007
Vrushti-coding <pvrushti71@gmail.com>
2024-11-14 11:30:01 +0530

    Add

--- project_1.java
+++ project_1.java
@@ -2,6 +2,9 @@ package projects;
 
 public class project_1 {
     public static void main(String[] args) {
+        int a=1,b=2;
+        int c=a*b;
+        System.out.println(c);
         System.out.println("hello");
     }
 }
